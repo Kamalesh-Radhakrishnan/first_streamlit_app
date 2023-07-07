5f43ef71a99b2a22355628982ffdabf109d0f29f
Kamalesh-Radhakrishnan <125634379+Kamalesh-Radhakrishnan@users.noreply.github.com>
2023-07-07 11:10:25 +0530

    Update streamlit_app.py
    Showing the complete details of the fruits selected

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,6 @@ streamlit.text('Poori Masal')
 streamlit.header('🍌🍑 Create your own smoothie here! 🍎🥝')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-fruits_to_show = streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index), ['Apple','Lemon'])
+fruits_selected = streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index), ['Apple','Lemon'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
