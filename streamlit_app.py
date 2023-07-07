73852271b3346f1dad205131351dfaf06f9fcbcb
Kamalesh-Radhakrishnan <125634379+Kamalesh-Radhakrishnan@users.noreply.github.com>
2023-07-07 09:50:24 +0530

    Update streamlit_app.py
    Added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Pongal Sambar')
+streamlit.text('Podi Idly')
+streamlit.text('Poori Masal')
